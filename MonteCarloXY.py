ed5fed967a10aa52e9cb76608468b427fbd162bd
liya <liyachar@gmail.com>
2025-03-28 16:42:55 +0100

    Structuur kleine update

--- MonteCarloXY.py
+++ MonteCarloXY.py
@@ -9,16 +9,33 @@ import numpy as np
 from matplotlib.animation import FuncAnimation 
 
 class Box():
-    def __init__(self,N=10, steps=100, T=1):
+    def __init__(self,N=10, steps=100, T=1, seed=3823582):
         self.N = N #grid size 1D
         self.steps = steps #timesteps
         self.T = T #temperature
 
-        self.spins = np.random.uniform(-np.pi, np.pi, [N,N]) #initializing spin states on grid
+        
         # self.spins = np.zeros([N,N])
         self.X, self.Y = np.meshgrid(np.arange(0,self.N), np.arange(0, self.N)) #2D grid lattice NxN
         self.energies = np.zeros(steps) #to store energies of the system at each t-step
         self.ti = 0     # index of last accepted step
+
+        self.set_init_conditions(seed=seed)
+
+
+    def set_init_conditions(self, rnd=True, abs_zero=False, seed=3823582):
+        # TODO: decide logical way to create init state, options:
+            # random random seed
+            # random set seed
+            # absolute 0
+        if abs_zero:
+            return
+        if rnd:
+            rng = np.random.default_rng()
+        else:
+            rng = np.random.default_rng(seed=seed)
+        self.spins = rng.uniform(-np.pi, np.pi, [self.N,self.N])
+
 
     def inner_prod(self, si, sj):
         """
@@ -27,7 +44,7 @@ class Box():
         """
         return -1 * (np.cos(si)*np.cos(sj) + np.sin(si)*np.sin(sj))
 
-    def Hamiltonian(self, x,y,val):
+    def Hamiltonian(self, x,y, val):
         """
         Computes the local energy sum of interactions of the 4 nearest
         neighbouring spins, using periodic boundary conditions. 
@@ -40,39 +57,36 @@ class Box():
 
         return summ
 
-    # def get_state_or_smt
-
-
-    def metropolis_1(self):
-        """
-        Computes the Metropolis algorithm for one position on the grid. 
-        """
-        ti = self.ti
-        x,y = np.random.choice(self.N, size=2) #picks random x,y (site) from grid
-        curr = self.spins[x,y] #spins on the random grid point
-        new = np.random.uniform(-np.pi, np.pi, 1) #new spin state
-        H1 = self.Hamiltonian(x,y,curr) #Hamiltonians of current and new spin states
+    def try_new_state(self):
+        ''' Try 1 new position '''
+        x,y = np.random.choice(self.N, size=2)
+        curr = self.spins[x,y]
+        new = np.random.uniform(-np.pi, np.pi, 1)
+        H1 = self.Hamiltonian(x,y,curr)
         H2 = self.Hamiltonian(x,y,new)
 
-        dE = H2 - H1 #Difference in the energies of 2 systems 
-        # print(dE)
-
-        # add acceptance logic
+        dE = H2 - H1
         p = np.exp(-1/self.T * dE) 
-        # print(p)
-        # p = 2
 
-        if H2 < H1:
-            # print('New E is lower, accept the move!')
-            self.spins[x,y] = new
-            self.energies[ti+1] = self.energies[ti] + dE
-            self.ti += 1 
-            
-        elif np.random.rand() < p: #dit snap ik niet? moet het niet zijn <?
-            # print('New E is higher, accept with certain probability!')
-            self.spins[x,y] = new
-            self.energies[ti+1] = self.energies[ti] + dE
-            self.ti += 1
+        if (H2 < H1) or (np.random.rand() < p):
+            return new, (x,y), dE
+        else:
+            return False, False, False
+
+    def step_metropolis(self):
+        '''
+        Perform 1 Metropolis step: try new states until one satifsies 
+        requirements.
+        '''
+        ti = self.ti
+
+        new_state = False
+        while not new_state:
+            new_state, pos, dE = self.try_new_state()
+
+        self.spins[pos] = new_state
+        self.energies[ti+1] = self.energies[ti] + dE
+        self.ti += 1
 
     def magnetization(self):
         pass
@@ -85,12 +99,12 @@ class Box():
         Performs NxN Metropolis steps, so for every site in the grid.
         """
         for i in range(self.N ** 2):
-            self.metropolis_1()
+            self.step_metropolis()
 
     def plot_energy(self):
         plt.figure()
         plt.plot(self.energies)
-        plt.savefig('energy2.png')
+        plt.savefig('energy.png')
 
     def state(self):
         U = np.cos(self.spins)
@@ -108,9 +122,10 @@ class Box():
         plt.savefig('state.png')
 
     def run(self):
-        while self.ti+1 < self.steps:
-        # for time_step in range(1, self.steps):
-            self.metropolis_1()
+        print('running...')
+        # while self.ti+1 < self.steps:
+        for time_step in range(1, self.steps):
+            self.step_metropolis()
             self.state()
 
 def animated(box):
@@ -121,25 +136,15 @@ def animated(box):
     quiv = ax.quiver(box.X, box.Y, a,c,d, pivot='mid') 
 
     def animate(i):
-        # print(i)
         box.sweep
         update_every = 5
-        
 
-        old_ti = box.ti
-        # print(i)
         if box.ti+1 >= box.steps:
-            return quiv,
-        while box.ti == old_ti:
-            box.metropolis_1()
-            # box.state()
-            # print('cur ti: ', box.ti)
-            # print('old ti: ', old_ti)
-
-        # print('exit while')
-
-        a, c, d = box.state()
-        quiv.set_UVC(a,c,d) 
+            pass
+        else:
+            box.step_metropolis()
+            a, c, d = box.state()
+            quiv.set_UVC(a,c,d) 
         
         return quiv,
 
@@ -159,13 +164,9 @@ def normal(box):
 
 def main():
     N = 20
-    steps = 1000
-    box = Box(N=N, steps=steps, T=0.5)
+    steps = 5000
+    box = Box(N=N, steps=steps, T=2.5, seed=12)
 
-    #box.metropolis_1(1)
-
-    #box.plot_state
-    #box.plot_energy
     #animated(box)
     normal(box)
 
